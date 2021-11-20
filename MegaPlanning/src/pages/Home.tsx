14bc3d4429e7123968de7840ab03615d218fc6e7
Adrien <adzerty76@outlook.fr>
2021-11-20 17:52:23 +0100

    Feat : ajout de la Home page

--- MegaPlanning/src/pages/Home.tsx
+++ MegaPlanning/src/pages/Home.tsx
@@ -1,22 +1,67 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonGrid, IonHeader, IonInput, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import { useEffect, useState } from 'react';
+import { Http, HttpResponse } from '@capacitor-community/http';
 
 const Home: React.FC = () => {
+
+  const ical = require('cal-parser');
+  const axios = require('axios');
+
+  const [link, setLink] = useState(""+localStorage.getItem("calendarLink"));
+
+
+
+  // Example of a GET request
+    const doGet = async () => {
+      const options = {
+        url: link,
+        headers: { 'X-Fake-Header': 'Max was here' },
+        params: { size: 'XL' },
+      };
+
+      const response: HttpResponse = await Http.get(options);
+
+      console.log(response.data);
+
+      // or...
+      // const response = await Http.request({ ...options, method: 'GET' })
+    };
+
+    doGet();
+    
+  
+  //console.log(myCalendarString);
+
+  let parsedCal = ical.parseString("");
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Mega Planning</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+          <IonGrid className="container">
+            <IonRow>
+              <IonLabel className="label_ftc"> Lien </IonLabel>
+              <IonInput className="input_ftc" 
+                        placeholder="Lien du calendrier..."  
+                        onIonChange={e => setLink(e.detail.value!)}>
+
+              </IonInput>
+            </IonRow>
+            <IonRow>
+              <IonButton onClick={()=>{
+                if(link !== "null")
+                {
+                  localStorage.setItem("calendarLink", link);
+                }
+              }}>Charger le calendrier</IonButton>
+              <IonButton className="secondary">Tutoriel</IonButton>
+            </IonRow>
+          </IonGrid> 
       </IonContent>
     </IonPage>
   );
